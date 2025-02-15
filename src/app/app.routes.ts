0f2c6c0f217a85a48139551de3c08694d25f0e2d
SpaceDraney <merandova@gmail.com>
2025-02-15 13:40:34 +0100

    Add routing configuration for home, game, and instructions pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'instructions', component: InstructionsComponent },
+  { path: '**', redirectTo: '' }, // Redirect any unknown paths to home
+];
